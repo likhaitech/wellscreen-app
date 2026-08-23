0a93d2e2f536a180d24ce4587a5310729dc559ba
Rosell,Yasser <RosellFirstGit@Gmail.com>
2026-08-24 04:41:14 +0800

    Fix Firebase widget test setup

--- mobile_app/test/widget_test.dart
+++ mobile_app/test/widget_test.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:app/main.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setupFirebaseCoreMocks();
+
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+
   testWidgets('WellScreen app starts and moves to login screen', (
     WidgetTester tester,
   ) async {
@@ -24,6 +34,7 @@ void main() {
 
     // The Create Account button is lower in the scrollable login screen.
     await tester.drag(find.byType(ListView), const Offset(0, -400));
+
     await tester.pumpAndSettle();
 
     expect(find.text('Create Account'), findsOneWidget);
